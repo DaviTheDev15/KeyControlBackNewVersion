fe509aafcd802a9c8ebe9b3ac530fee8667491a1
DaviTheDev15 <antoniodavidsouza13@gmail.com>
2026-04-15 19:59:23 -0300

    Modificacao em Helpers/application

--- helpers/application/__init__.py
+++ helpers/application/__init__.py
@@ -3,7 +3,7 @@ from flask_restful import Api
 from flask_jwt_extended import JWTManager
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:Souza30072005@localhost:5432/keycontrol"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:Souza30072005@keycontrol-db:5432/keycontrol"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["JWT_SECRET_KEY"] = "super-secret-key"
 app.config["JWT_ACCESS_TOKEN_EXPIRES"] = 3600
