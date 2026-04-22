600db8a78ad81dc893bbf325d9f6d431346ffbe9
DaviTheDev15 <antoniodavidsouza13@gmail.com>
2026-04-22 10:18:29 -0300

    Correcao em ResponsavelSchemaValidation

--- helpers/validation_functions/responsavelSchemaValidation.py
+++ helpers/validation_functions/responsavelSchemaValidation.py
@@ -1,14 +1,15 @@
 from marshmallow import ValidationError
-from models.TB_Responsavel import TB_Responsavel
 from helpers.database import db
 
 def validar_unique_cpf(value):
+    from models.TB_Responsavel import TB_Responsavel
     if db.session.query(TB_Responsavel).filter_by(responsavel_cpf=value).first():
         raise ValidationError(
             "Já existe um Responsavel cadastrado com esse CPF."
         )
 
 def validar_unique_siap(value):
+    from models.TB_Responsavel import TB_Responsavel
     if db.session.query(TB_Responsavel).filter_by(responsavel_siap=value).first():
         raise ValidationError(
             "Já existe um Responsavel cadastrado com esse SIAP."
