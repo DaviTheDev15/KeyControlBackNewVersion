677c816311d3a3e46437d867da9532f7eaa6cd07
DaviTheDev15 <antoniodavidsouza13@gmail.com>
2026-07-23 14:36:15 -0300

    Modificação em salas

--- models/Sala.py
+++ models/Sala.py
@@ -1,14 +1,17 @@
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from sqlalchemy import String, Integer, Boolean
+from sqlalchemy import String, Integer, Boolean, DateTime, func
 from helpers.database import db
 from helpers.validation_functions.genericValidations import montarDicionarioDeMensagemDeErro
 from marshmallow import Schema, fields, validate, validates
 from flask_restful import fields as flaskFields
+from datetime import datetime
 
 tb_sala_fields = {
     'sala_id': flaskFields.Integer,
     'sala_nome': flaskFields.String,
-    'disponivel': flaskFields.Boolean
+    'disponivel': flaskFields.Boolean,
+    'created_at': flaskFields.DateTime,
+    'deleted_at': flaskFields.DateTime
 }
 
 class TB_Sala(db.Model):
@@ -17,6 +20,8 @@ class TB_Sala(db.Model):
     sala_id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
     sala_nome: Mapped[str] = mapped_column(String(255), nullable=False)
     disponivel: Mapped[bool] = mapped_column(Boolean, nullable=False)
+    created_at: Mapped[datetime] = mapped_column(DateTime, server_default=func.now(), nullable=False)
+    deleted_at: Mapped[datetime] = mapped_column(DateTime, nullable=True)
 
     tb_chave = relationship("TB_Chave", back_populates="tb_sala")
     tb_reserva = relationship("TB_Reserva", back_populates="tb_sala")
@@ -33,3 +38,8 @@ class TB_SalaSchema(Schema):
     disponivel = fields.Boolean(
         required=True,
         error_messages=montarDicionarioDeMensagemDeErro("disponivel", ["required", "invalid"]))
+    
+    created_at = fields.DateTime(
+        required=True,
+        error_messages=montarDicionarioDeMensagemDeErro("created_at",["required", "null"])
+    )
